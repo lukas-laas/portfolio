53bc727b75e5915f7808ca72e57b09ac1b4aa271
lukas-laas <lukas.s.laas@gmail.com>
2024-08-07 15:50:02 +0200

    add links

--- src/app/_components/projects/brick-dashboard.tsx
+++ src/app/_components/projects/brick-dashboard.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import { Article, Gallery, Header3, Paragraph } from "../ui";
 
 export function BrickDashboard() {
   return (
     <Article>
-      <Header3>Project!</Header3>
+      <Header3>
+        <Link
+          href="https://www.brick.tech/en-us"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Brick Dashboard
+        </Link>
+      </Header3>
       <Gallery
         images={[
           { url: "/image (1).png", title: "cat", alt: "cat" },
@@ -14,9 +23,18 @@ export function BrickDashboard() {
         ]}
       />
       <Paragraph>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae quia
-        sunt maiores repudiandae non, earum labore accusantium facere repellat
-        ea?
+        The internal dashboard of{" "}
+        <Link
+          href="https://www.brick.tech/en-us"
+          className="underline"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Brick Technology
+        </Link>
+        , I remade a couple of pages based on new designs and added a system for
+        uploading pictures for their venues to AWS S3. I also added a function
+        for checking the health of batteries and a page for viewing articles.
       </Paragraph>
     </Article>
   );
