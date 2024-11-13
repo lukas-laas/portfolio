f4b5526510625a0f3d944e5ea20f8c7b1b2175e2
lukas-laas <lukas.s.laas@gmail.com>
2024-11-13 13:59:59 +0100

    add venue map project

--- src/app/_components/projects.tsx
+++ src/app/_components/projects.tsx
@@ -1,6 +1,7 @@
 import { BrickDashboard } from "./projects/brick-dashboard";
 import { MusicWebsite } from "./projects/music-website";
 import { SlowMessagingApp } from "./projects/slow-messaging-app";
+import { VenueMap } from "./projects/venue-map";
 import { Divider, Header2 } from "./ui";
 
 export function Projects() {
@@ -10,6 +11,7 @@ export function Projects() {
         <Header2>Projects</Header2>
         <Divider />
       </header>
+      <VenueMap />
       <SlowMessagingApp />
       <MusicWebsite />
       <BrickDashboard />
---
+++ src/app/_components/projects/venue-map.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { Article, Gallery, Header3, Paragraph } from "../ui";
+
+export function VenueMap() {
+  return (
+    <Article>
+      <Link
+        href="https://venue-map.vercel.app"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <Header3>Venue map</Header3>
+      </Link>
+      <Gallery
+        images={[
+          {
+            url: "/venue-map/Screenshot from 2024-11-13 13-55-22.png",
+            title: "Venue map",
+            alt: "Venue map",
+          },
+          {
+            url: "/venue-map/Screenshot from 2024-11-13 13-55-37.png",
+            title: "Venue map",
+            alt: "Venue map",
+          },
+          {
+            url: "/venue-map/Screenshot from 2024-11-13 13-55-47.png",
+            title: "Venue map",
+            alt: "Venue map",
+          },
+        ]}
+      />
+      <Paragraph>
+        A web app to crowd source data about music venues. The goal was to make
+        it easier for smaller artists and concert goers to keep track of music
+        venues. At the moment the user submits data and I add and update venues
+        through the admin panel.
+      </Paragraph>
+    </Article>
+  );
+}
